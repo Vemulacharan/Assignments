0bd428f22d33e0fcc1fbaf37979a726a619d5619
VEMULA CHARAN <103623843+Vemulacharan@users.noreply.github.com>
2023-10-17 10:11:37 +0530

    Add files via upload

---
+++ Task 5/sumOfArray.java
@@ -0,0 +1,26 @@
+package sampleprograms;
+
+import java.util.Scanner;
+
+public class sumOfArray {
+	public static void main(String[] args) {
+		int[] a = new int[5];
+		int  sum=0;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entet an Array");
+		
+		for(int i=0; i<a.length; i++) {
+			a[i]=sc.nextInt();
+		}
+	
+		for(int i=0; i<a.length; i++) {
+			sum+=a[i];
+		}
+		System.out.println("The sum of an array: "+sum);
+		System.out.println("The average of an array: "+sum/a.length);
+		sc.close();
+	}
+	
+
+}
